3f6210ddafa96a603488068632188af8a1f63241
danielian1121 <danielian1121@gmail.com>
2018-12-13 13:57:58 +0800

    Create table

--- server.js
+++ server.js
@@ -16,23 +16,32 @@ server.get('/api', (req, res) => {
   try {
     const time = JSON.parse(req.query.time)
     const day = JSON.parse(req.query.day)
+    if (time.length !== day.length)
+      throw new Error('Query length not equal.')
     const option = []
-    for (let i = 0; i < time.length; i++) {
+    const length = day.length
+    for (let i = 0; i < length; i++) {
       option.push({
-        time: time[i],
         day: day[i]
       })
     }
     mongoClient.then(db => {
         const dbo = db.db("heroku_d0snmc5z")
         return dbo.collection("nckua9").find({
-          $or: option,
+          $or: option
         }).toArray()
       }).then(result => {
-        res.send(result)
+        const answer = []
+        result.map((data, index) => {
+          for (let i = 0; i < length; i++) {
+            if (time[i].match(data.time) && day[i].match(data.day))
+              answer.push(data)
+          }
+        })
+        res.send(answer)
       })
   } catch (e) {
-    res.status(500)
-    res.send('Error')
+    res.status(400)
+    res.send(e.message)
   }
 })

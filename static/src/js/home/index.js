5e9b9b20b5cc9389ce541e628d5dc12022e84397
danielian1121 <danielian1121@gmail.com>
2018-12-17 19:13:22 +0800

    Fix bug: prevent clicking button with no select time

--- static/src/js/home/index.js
+++ static/src/js/home/index.js
@@ -27,37 +27,45 @@ button.addEventListener('click', () => {
   let day = [1,2,3,4,5]
   const click = document.getElementsByClassName('row__element--active')
   const array = [...click]
-  array.map(value => {
-    const data = String(value.cellIndex)
-    const nowtime = value.parentElement.firstChild.textContent
-    switch (data) {
-      case '1':
-        time[0] += nowtime
-        break
-      case '2':
-        time[1] += nowtime
-        break
-      case '3':
-        time[2] += nowtime
-        break
-      case '4':
-        time[3] += nowtime
-        break
-      case '5':
-        time[4] += nowtime
-        break
+  if (!array.length){
+    Swal({
+      type: 'error',
+      title: 'Oops...',
+      text: '你沒有選擇課程時段'
+    })
+  }else {
+    array.map(value => {
+      const data = String(value.cellIndex)
+      const nowtime = value.parentElement.firstChild.textContent
+      switch (data) {
+        case '1':
+          time[0] += nowtime
+          break
+        case '2':
+          time[1] += nowtime
+          break
+        case '3':
+          time[2] += nowtime
+          break
+        case '4':
+          time[3] += nowtime
+          break
+        case '5':
+          time[4] += nowtime
+          break
+      }
+    })
+    while (time.indexOf('') !== -1) {
+      day.splice(time.indexOf(''),1)
+      time.splice(time.indexOf(''),1)
     }
-  })
-  while (time.indexOf('') !== -1) {
-    day.splice(time.indexOf(''),1)
-    time.splice(time.indexOf(''),1)
+    let href = '/api?'
+    let hrefTime = 'time=['
+    let hrefDay = 'day=['
+    for (let i = 0; i < day.length; i++) {
+      hrefTime += `\"${time[i]}\",`
+      hrefDay += `\"${day[i]}\",`
+    }
+    location.href = `${href}${hrefTime.slice(0,-1)}]&${hrefDay.slice(0,-1)}]`
   }
-  let href = '/api?'
-  let hrefTime = 'time=['
-  let hrefDay = 'day=['
-  for (let i = 0; i < day.length; i++) {
-    hrefTime += `\"${time[i]}\",`
-    hrefDay += `\"${day[i]}\",`
-  }
-  location.href = `${href}${hrefTime.slice(0,-1)}]&${hrefDay.slice(0,-1)}]`
 })

38c2bb3bea3b8c4ed5b08e325365bcd22f19fe40
danielian1121 <danielian1121@gmail.com>
2018-12-30 23:42:33 +0800

    Finish large version of result page

--- static/src/js/result/index.js
+++ static/src/js/result/index.js
@@ -10,4 +10,31 @@ fetch(`${location.protocol}//${location.host}/api${query}`)
     const value = data.data
     console.log(value)
     target[0].innerHTML = content( {value} )
+    clickEvent()
   })
+
+function clickEvent () {
+  const list = document.getElementsByClassName('tbody__rows')
+  let listArray = []
+  for (let i = 1; i < list.length; i++)
+    listArray.push(list[i])
+  listArray.map(value => {
+    value.addEventListener('click', () => {
+      const subList = []
+      let temp = value.nextSibling
+      while (temp.classList.contains('tbody__history')) {
+        subList.push(temp)
+        temp = temp.nextSibling
+      }
+      if (subList.length === 0){}
+      else if (subList[0].classList.contains('tbody__history--active'))
+        subList.map(value => {
+          value.classList.remove('tbody__history--active')
+        })
+      else
+        subList.map(value => {
+          value.classList.add('tbody__history--active')
+        })
+    })
+  })
+}

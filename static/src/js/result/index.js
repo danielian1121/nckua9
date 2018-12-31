43d673ade66b7c68d4bca00d19ae976d2afba1c9
danielian1121 <danielian1121@gmail.com>
2018-12-31 13:24:48 +0800

    Finish small version of result page

--- static/src/js/result/index.js
+++ static/src/js/result/index.js
@@ -4,37 +4,56 @@ const content = require('../../pug/common/result/content.pug')
 
 const target = document.getElementsByClassName('content')
 
+let width = innerWidth
+
 fetch(`${location.protocol}//${location.host}/api${query}`)
 .then( res => res.json())
   .then( data => {
     const value = data.data
-    console.log(value)
     target[0].innerHTML = content( {value} )
-    clickEvent()
+    if (width > 720)
+      clickEvent()
   })
+
+window.onresize = () => {
+  if (width <= 720 && innerWidth > 720) {
+    width = innerWidth
+    clickEvent()
+  }
+  else if (width > 720 && innerWidth <= 720) {
+    width = innerWidth
+    const list = document.getElementsByClassName('tbody__rows')
+    let listArray = [...list]
+    listArray.map(value => {
+      value.removeEventListener('click', click)
+    })
+  }
+}
 
 function clickEvent () {
   const list = document.getElementsByClassName('tbody__rows')
-  let listArray = []
-  for (let i = 1; i < list.length; i++)
-    listArray.push(list[i])
+  let listArray = [...list]
   listArray.map(value => {
-    value.addEventListener('click', () => {
-      const subList = []
-      let temp = value.nextSibling
-      while (temp.classList.contains('tbody__history')) {
-        subList.push(temp)
-        temp = temp.nextSibling
-      }
-      if (subList.length === 0){}
-      else if (subList[0].classList.contains('tbody__history--active'))
-        subList.map(value => {
-          value.classList.remove('tbody__history--active')
-        })
-      else
-        subList.map(value => {
-          value.classList.add('tbody__history--active')
-        })
-    })
+    value.addEventListener('click', click)
+    value.argument = value
   })
+}
+
+function click (evt) {
+  const subList = []
+  let temp = evt.target.parentNode.argument.nextSibling
+  while (temp && temp.classList.contains('tbody__history')) {
+    subList.push(temp)
+    temp = temp.nextSibling
+  }
+  console.log()
+  if (subList.length === 0){}
+  else if (subList[0].classList.contains('tbody__history--active'))
+    subList.map(value => {
+      value.classList.remove('tbody__history--active')
+    })
+  else
+    subList.map(value => {
+      value.classList.add('tbody__history--active')
+    })
 }
